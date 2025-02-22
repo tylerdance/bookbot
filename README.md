5a8477b5bc7a5f44718316e5ea454f1d7f151932
tylerdance <72777766+tylerdance@users.noreply.github.com>
2025-02-21 21:43:17 -0500

    readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # bookbot
-BookBot is my first project!
+hello world
---
+++ main.py
@@ -0,0 +1,12 @@
+path_to_file = "books/frankenstein.txt"
+
+def main():
+    with open(path_to_file) as f:
+        file_contents = f.read()
+        print(file_contents)
+
+
+def number_of_words(string):
+    words = 
+
+main()
